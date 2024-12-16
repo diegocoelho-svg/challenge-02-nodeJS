ca5395393622f2348b29f54adee781c49fd3515e
Diego Coelho <diegocoelhoaprova@gmail.com>
2024-12-16 19:41:08 -0300

    edit migration users

--- db/migrations/20241216221005_create-documents.ts
+++ db/migrations/20241216221005_create-documents.ts
@@ -1,10 +1,15 @@
-import type { Knex } from "knex";
-
+import type { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('users', (table) => {
+    table.uuid('id').primary() // define uma coluna id (uuid) e primary configura id como a chave primaria da primeira tabela, garantindo q seja unico.
+    table.string('session_id').notNullable().unique() // cria coluna para armazenar identificação / valor obrigatório / único sem duplicata
+    table.string('name').notNullable()
+    table.string('email').notNullable().unique()
+    table.timestamps(true, true).notNullable()
+  })
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('users')
 }
-
